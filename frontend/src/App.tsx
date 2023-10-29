b8f804e41b78f274751bacc527fa8b71fe686ac2
NinaNikazm <nikazm.nina@gmail.com>
2023-10-29 00:48:31 -0400

    UP TO NAVIGATION LAB 3 FIXED

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,30 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
 import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import Home from "./Home/Home";
 import Products from "./Products/Products";
 
+import "./App.css";
+
+
 function App() {
+  const openMenu = () => {
+    document.querySelector(".sidebar")?.classList.add("open");
+  };
+
+  const closeMenu = () => {
+    document.querySelector(".sidebar")?.classList.remove("open");
+  };
+
   return (
     <Router>
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <div className="grid-container">
+          <header className="header">
+            <div className="brand">
+              <button onClick={openMenu}>&#9776;</button>
+              <a href="index.html">The Girls In Tech</a>
+            </div>
+            <div className="header-links">
+              <Link to="/">Home</Link>
+              <Link to="/catalog">Catalog</Link>
+              <Link to="/orders">Orders</Link>
+              <Link to="/payment">Payment</Link>
+              <Link to="/fulfillment">Fulfillment</Link>
+            </div>
+          </header>
+          <div>
+            <aside className="sidebar">
+              <h3>Shopping Categories</h3>
+              <button className="sidebar-close-button" onClick={closeMenu}>
+                x
+              </button>
+              <ul>
+                <li>
+                  <a href="index.html">Pants</a>
+                </li>
+
+                <li>
+                  <a href="index.html">Shirts</a>
+                </li>
+              </ul>
+            </aside>
+            <main className="main">
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/catalog" element={<Products />} />
+
+              </Routes>
+            </main>
+            <footer>&copy; 2023 The Girls In Tech</footer>
+          </div>
+        </div>
+      </div>
     </Router>
   );
 }
